2787905d427e82a57650954a3f9d00527f83a8a8
AdelSehic <adelsehic95@gmail.com>
2023-08-22 16:45:55 +0200

    Goto removed. Invalid option propperly handled

--- agi.php
+++ agi.php
@@ -11,31 +11,36 @@ $agi->exec("PLAYBACK", "welcome");
 
 pwcheck($pass, $agi);
 
-ivr:
-$option = $agi->get_data("sekretarica", 20000, 1);
-switch ($option['result']){
-    case 1:
-        $exten = $agi->get_data("vm-extension", 20000);
-        if ($exten['result']==$caller){
-            $agi->exec("PLAYBACK", "invalid");
-            $agi->hangup();
-        }
-        $agi->set_variable("tocall", $exten['result']);
-        $agi->set_variable("redirect", "1");
-        break;
-    case 2:
-        pwcheck($pass, $agi);
-        $pw1 = $agi->get_data("vm-newpassword", 20000, 4);
-        $pw2 = $agi->get_data("vm-reenterpassword", 20000, 4);
-        if ($pw1 != $pw2 ){
-            $agi->exec("PLAYBACK", "vm-mismatch");
-            $agi->hangup();
-        }
-        $agi->database_put("password", $caller, $pw1['result']);
-        $agi->exec("PLAYBACK", "vm-passchanged");
-        break;
-    case 3:
-        goto ivr;
+for($exit = false; !$exit;){
+    $option = $agi->get_data("sekretarica", 20000, 1);
+    switch ($option['result']){
+        case 1:
+            $exten = $agi->get_data("vm-extension", 20000);
+            if ($exten['result']==$caller){
+                $agi->exec("PLAYBACK", "invalid");
+                $agi->hangup();
+            }
+            $agi->set_variable("tocall", $exten['result']);
+            $agi->set_variable("redirect", "1");
+            $exit = true;
+            break;
+        case 2:
+            pwcheck($pass, $agi);
+            $pw1 = $agi->get_data("vm-newpassword", 20000, 4);
+            $pw2 = $agi->get_data("vm-reenterpassword", 20000, 4);
+            if ($pw1 != $pw2 ){
+                $agi->exec("PLAYBACK", "vm-mismatch");
+                $agi->hangup();
+            }
+            $agi->database_put("password", $caller, $pw1['result']);
+            $agi->exec("PLAYBACK", "vm-passchanged");
+            $exit = true;
+            break;
+        case 3:
+            $exit = false;
+        default:
+            $agi->exec("PLAYBACK","confbridge-invalid");
+    }
 }
 
 function pwcheck($pass, $agi){
